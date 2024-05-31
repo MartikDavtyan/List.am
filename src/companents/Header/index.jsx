97ff566976bb89c21b5732ed93d1aa1dcc9c1316
Martik <martikdavtyanait@gmail.com>
2024-05-31 16:42:20 +0400

    Import Header Left and Right parts

--- src/companents/Header/index.jsx
+++ src/companents/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../../companents-css/Header/header.css'
+import HeaderLeft from './HeaderLeft'
+import HeaderRight from './HeaderRight'
 
 export default function Header() {
   return (
     <header>
-
+      <HeaderLeft />
+      <HeaderRight />
     </header>
   )
 }
