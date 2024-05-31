16f8d1224300f20d3991530ee0452da5bd3f81f2
Martik <martikdavtyanait@gmail.com>
2024-06-01 02:00:51 +0400

    Finish story

--- src/companents/Story/StoryEl.jsx
+++ src/companents/Story/StoryEl.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../../companents-css/Story/storyEl.css'
+import React from 'react';
+import '../../companents-css/Story/storyEl.css';
 
-export default function StoryEl({bc,c,img,text}) {
-  return (
-    <a class="story-c" style={{ backgroundColor: {bc}, color: {c} }}>
-        <div class="story-divs" style={{ backgroundImage: {img} }}>
-            <div class="story-div2">{text}</div>
-        </div>
-    </a>
-  )
+export default function StoryEl({ bc, c, img, text }) {
+    return (
+        <a className="story-c" style={{ backgroundColor: bc, color: c }}>
+            <div className="story-divs" style={{ backgroundImage: img }}>
+                <div className="story-div2">{text}</div>
+            </div>
+        </a>
+    );
 }
--- src/companents/Story/index.jsx
+++ src/companents/Story/index.jsx
@@ -1,68 +1,72 @@
-import React from 'react'
-import '../../companents-css/Story/story.css'
+import React from 'react';
+import '../../companents-css/Story/story.css';
+import StoryEl from './StoryEl.jsx';
 
 export default function Story() {
-    const StoryBassa=[
+    const storyBassa = [
         {
-            key:1,
-            bc:'#009dcf',
-            c:'#fff',
-            img:'url()',
-            text:'Օրավարձով բնակարաններ',
+            key: 1,
+            bc: '#009dcf',
+            c: '#fff',
+            img: 'url(https://sa.list.am/220.png)',
+            text: 'Օրավարձով բնակարաններ',
         },
         {
-            key:2,
-            bc:'#CDE6CC',
-            c:'#ffffff',
-            img:'url()',
-            text:'Նոթբուքի պայուսակներ',
+            key: 2,
+            bc: '#CDE6CC',
+            c: '#ffffff',
+            img: 'url(https://sa.list.am/328.png)',
+            text: 'Նոթբուքի պայուսակներ',
         },
         {
-            key:3,
-            bc:'#93BBEE',
-            c:'#ffffff',
-            img:'url(https://sa.list.am/220.png)',
-            text:'Արտադրված է Հայաստանում',
+            key: 3,
+            bc: '#93BBEE',
+            c: '#ffffff',
+            img: 'url(https://sa.list.am/220.png)', // Update with actual image URL
+            text: 'Արտադրված է Հայաստանում',
         },
         {
-            key:4,
-            bc:'#D6C3E8',
-            c:'#000',
-            img:'url(https://sa.list.am/328.png)',
-            text:'Խթանեք Ձեր բիզնեսը',
+            key: 4,
+            bc: '#D6C3E8',
+            c: '#000',
+            img: 'url(https://sa.list.am/328.png)', // Update with actual image URL
+            text: 'Խթանեք Ձեր բիզնեսը',
         },
         {
-            key:5,
-            bc:'#FAE56E',
-            c:'#000',
-            img:'url(https://sa.list.am/329.png)',
-            text:'Սեղանի խաղեր',
+            key: 5,
+            bc: '#FAE56E',
+            c: '#000',
+            img: 'url(https://sa.list.am/329.png)', // Update with actual image URL
+            text: 'Սեղանի խաղեր',
         },
         {
-            key:6,
-            bc:'#D9D7CE',
-            c:'#000',
-            img:'url(https://sa.list.am/311.png)',
-            text:'Տղամարդկանց կոշիկներ',
+            key: 6,
+            bc: '#D9D7CE',
+            c: '#000',
+            img: 'url(https://sa.list.am/311.png)', // Update with actual image URL
+            text: 'Տղամարդկանց կոշիկներ',
         },
         {
-            key:7,
-            bc:'#e57d15',
-            c:'#fff',
-            img:'url(https://sa.list.am/221.png)',
-            text:'List.am-ում անվտանգություն',
+            key: 7,
+            bc: '#e57d15',
+            c: '#fff',
+            img: 'url(https://sa.list.am/221.png)', // Update with actual image URL
+            text: 'List.am-ում անվտանգություն',
         },
         {
-            key:8,
-            bc:'#006aff',
-            c:'#ffffff',
-            img:'url(https://sa.list.am/215.png)',
-            text:'Էլեկտրական մեքենաներ',
+            key: 8,
+            bc: '#006aff',
+            c: '#ffffff',
+            img: 'url(https://sa.list.am/215.png)', // Update with actual image URL
+            text: 'Էլեկտրական մեքենաներ',
         }
-    ]
-  return (
-    {
+    ];
 
-    }
-  )
+    return (
+        <div className="c">
+            {storyBassa.map((el) => (
+                <StoryEl key={el.key} bc={el.bc} c={el.c} img={el.img} text={el.text} />
+            ))}
+        </div>
+    );
 }
