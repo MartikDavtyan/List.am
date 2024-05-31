4bf0e6ffbb3c2d744201ecca1a6877f009c5a756
Martik <martikdavtyanait@gmail.com>
2024-05-31 16:48:39 +0400

    Add header left code

--- src/companents/Header/HeaderLeft.jsx
+++ src/companents/Header/HeaderLeft.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function HeaderLeft() {
   return (
-    <div>HeaderLeft</div>
+    <>
+        <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/List.am_Logo.png/1112px-List.am_Logo.png" class="header-img"/>
+        <div class="header-div">
+            <img src="../assets/icon.png" class="header-icon" />
+            <p>Բաժիններ</p>
+        </div>
+    </>
   )
 }
