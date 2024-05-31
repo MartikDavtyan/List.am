5d9fa0b65247a066204680d2695b9af650c9cde0
Martik <martikdavtyanait@gmail.com>
2024-05-31 16:30:52 +0400

    Created Header css code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './companents/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return 
+  return (
+    <>
+      <Header />
+      <div className="main">
+      
+      </div>
+    </>
+  )
 }
 
 export default App
