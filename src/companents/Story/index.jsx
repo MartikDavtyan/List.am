e622dc1a471a0c1511590b3fcc069dc610a80776
Martik <martikdavtyanait@gmail.com>
2024-06-05 17:54:23 +0400

    Createing dataBassa for story

--- src/companents/Story/index.jsx
+++ src/companents/Story/index.jsx
@@ -3,64 +3,7 @@ import '../../companents-css/Story/story.css';
 import StoryEl from './StoryEl.jsx';
 
 export default function Story() {
-    const storyBassa = [
-        {
-            key: 1,
-            bc: '#009dcf',
-            c: '#fff',
-            img: 'url(https://sa.list.am/220.png)',
-            text: 'Օրավարձով բնակարաններ',
-        },
-        {
-            key: 2,
-            bc: '#CDE6CC',
-            c: '#ffffff',
-            img: 'url(https://sa.list.am/328.png)',
-            text: 'Նոթբուքի պայուսակներ',
-        },
-        {
-            key: 3,
-            bc: '#93BBEE',
-            c: '#ffffff',
-            img: 'url(https://sa.list.am/220.png)', // Update with actual image URL
-            text: 'Արտադրված է Հայաստանում',
-        },
-        {
-            key: 4,
-            bc: '#D6C3E8',
-            c: '#000',
-            img: 'url(https://sa.list.am/328.png)', // Update with actual image URL
-            text: 'Խթանեք Ձեր բիզնեսը',
-        },
-        {
-            key: 5,
-            bc: '#FAE56E',
-            c: '#000',
-            img: 'url(https://sa.list.am/329.png)', // Update with actual image URL
-            text: 'Սեղանի խաղեր',
-        },
-        {
-            key: 6,
-            bc: '#D9D7CE',
-            c: '#000',
-            img: 'url(https://sa.list.am/311.png)', // Update with actual image URL
-            text: 'Տղամարդկանց կոշիկներ',
-        },
-        {
-            key: 7,
-            bc: '#e57d15',
-            c: '#fff',
-            img: 'url(https://sa.list.am/221.png)', // Update with actual image URL
-            text: 'List.am-ում անվտանգություն',
-        },
-        {
-            key: 8,
-            bc: '#006aff',
-            c: '#ffffff',
-            img: 'url(https://sa.list.am/215.png)', // Update with actual image URL
-            text: 'Էլեկտրական մեքենաներ',
-        }
-    ];
+    const 
 
     return (
         <div className="c">
