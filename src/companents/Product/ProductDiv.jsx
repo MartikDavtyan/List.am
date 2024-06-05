25ba445ef7f34a0bc48edd7cb22332fa7af00aaf
Martik <martikdavtyanait@gmail.com>
2024-06-05 19:06:58 +0400

    import modal element

--- src/companents/Product/ProductDiv.jsx
+++ src/companents/Product/ProductDiv.jsx
@@ -8,7 +8,7 @@ export default function ProductDiv({name,arr}) {
     <p style={{fontSize:"20px"}}>{name}</p>
     <div className="products">
         {arr.map((el)=>{
-            return <ProductDivEl key={el.id} img={el.img} price={el.price} info_1={el.info_1} info_2={el.info_2} />
+            return <ProductDivEl key={el.id} img={el.img} price={el.price} info_1={el.info_1} info_2={el.info_2} userImg={el.userImg} userName={el.userName} text={el.text} info={el.info} finishInfo={el.finishInfo} time={el.time} />
         })}
     </div>
 </div>
