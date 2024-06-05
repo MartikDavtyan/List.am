a44ad74dda1a7db42ac98b761000a0789e07c8a2
Martik <martikdavtyanait@gmail.com>
2024-06-05 17:31:50 +0400

    Delete app.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './companents/Header'
-import Cotalog from './companents/Cotalog'
-import Story from './companents/Story'
-import Product from './companents/Product'
-import Footer from './companents/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
