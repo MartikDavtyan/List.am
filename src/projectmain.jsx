bf09ecf3b5ab654ba291b74843152cb62a7db05a
Martik <martikdavtyanait@gmail.com>
2024-06-05 17:33:06 +0400

    imports files to projectMain

---
+++ src/projectmain.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Header from './companents/Header'
+import Cotalog from './companents/Cotalog'
+import Story from './companents/Story'
+import Product from './companents/Product'
+import Footer from './companents/Footer'
+export default function Projectmain() {
+  return (
+    <>
+    
+    </>
+  )
+}
