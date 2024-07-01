b00965f0a0bc4f770ddb2cfc6d8bb07da0f90af4
Martik <martikdavtyanait@gmail.com>
2024-07-01 14:51:38 +0400

    add MyPage

--- src/projectmain.jsx
+++ src/projectmain.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Routes , Route , Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import MyPage from "./pages/MyPage"
 
 import Header from './companents/Header'
 export default function Projectmain() {  
@@ -9,6 +10,7 @@ export default function Projectmain() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/myPage' element={<MyPage />} />
       </Routes>
     </>
   )
