9e44d7b92fbd2480c6e04f4acc1ca8d3dcaaf9d8
Martik <martikdavtyanait@gmail.com>
2024-07-01 15:14:22 +0400

    Finish myPage css

--- src/companents/myPage/inde.jsx
+++ src/companents/myPage/inde.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function inde() {
   return (
-    <div className="page">
+    <div className="pageMain">
+        <div className="page">
         <h2>List.am Մուտք</h2>
         <div className="inputesMain">
             <input type="email" />
             <input type='password' />
         </div>
         <button>Մուտք</button>
+    </div>
     </div>
   )
 }
