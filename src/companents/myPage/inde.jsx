22d38e172c4025f3705539c5edda28a61e8f6cd5
Martik <martikdavtyanait@gmail.com>
2024-07-01 14:59:16 +0400

    Add login code

--- src/companents/myPage/inde.jsx
+++ src/companents/myPage/inde.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function inde() {
   return (
     <div className="page">
-            
+        <h2>List.am Մուտք</h2>
+        <div className="inputesMain">
+            <input type="email" />
+            <input type='password' />
+        </div>
+        <button>Մուտք</button>
     </div>
   )
 }
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Page from '../companents/myPage/inde'
 
 export default function MyPage() {
   return (
     <>
-        
+        <Page />
     </>
   )
 }
